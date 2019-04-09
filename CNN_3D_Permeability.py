16c41d8ec5ae713ae1e8949f79a8a47348aadc6d
Fajar Wimar Ramadhan <wr.fajar@gmail.com>
2019-04-09 10:32:47 +0700

    exp 16
    #Model
    Conv3D(16, kernel_size=5, strides=(2, 2, 2), activation='relu')
    Conv3D(16, kernel_size=3, strides=(1, 1, 1), activation='relu')
    MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1))
    Conv3D(16, kernel_size=3, strides=(1, 1, 1), activation='relu')
    Conv3D(16, kernel_size=3, strides=(1, 1, 1), activation='relu')
    MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1))
    Dense(128, activation='relu')
    Dense(128, activation='elu')
    Dense(64, activation='relu')
    Dense(1, activation=None)

--- CNN_3D_Permeability.py
+++ CNN_3D_Permeability.py
@@ -35,7 +35,7 @@ sys.path.append(os.getcwd())
 from DataGenerator_3D_Classes import DataGenerator
 
 #Experiment number
-exp_num = 15
+exp_num = 16
 os.chdir('..\\..\\005_Result\\CNN_3D')
 if any('00'+str(exp_num) in s for s in os.listdir(os.getcwd())):
     sys.exit('Alert : There is already 00'+str(exp_num)+' experiment result!!')
@@ -84,14 +84,14 @@ total_generator = DataGenerator(partition['total'], labels, **params)
 
 #Define a model
 model = Sequential()
-model.add(Conv3D(64, kernel_size=5, strides=(2, 2, 2), padding='valid',
+model.add(Conv3D(16, kernel_size=5, strides=(2, 2, 2), padding='valid',
                  data_format='channels_last', dilation_rate=(1, 1, 1),
                  activation='relu', use_bias=True,
                  kernel_initializer='glorot_uniform', bias_initializer='zeros',
                  kernel_regularizer=None, bias_regularizer=None, 
                  activity_regularizer=None, kernel_constraint=None,
                  bias_constraint=None, input_shape=(dim1, dim2, dim3, chn)))
-model.add(Conv3D(64, kernel_size=3, strides=(1, 1, 1), padding='valid',
+model.add(Conv3D(16, kernel_size=3, strides=(1, 1, 1), padding='valid',
                  data_format='channels_last', dilation_rate=(1, 1, 1),
                  activation='relu', use_bias=True,
                  kernel_initializer='glorot_uniform', bias_initializer='zeros',
@@ -100,14 +100,14 @@ model.add(Conv3D(64, kernel_size=3, strides=(1, 1, 1), padding='valid',
                  bias_constraint=None))
 model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1), padding='valid',
                        data_format='channels_last'))
-model.add(Conv3D(64, kernel_size=3, strides=(1, 1, 1), padding='valid',
+model.add(Conv3D(16, kernel_size=3, strides=(1, 1, 1), padding='valid',
                  data_format='channels_last', dilation_rate=(1, 1, 1),
                  activation='relu', use_bias=True,
                  kernel_initializer='glorot_uniform', bias_initializer='zeros',
                  kernel_regularizer=None, bias_regularizer=None, 
                  activity_regularizer=None, kernel_constraint=None,
                  bias_constraint=None))
-model.add(Conv3D(64, kernel_size=3, strides=(1, 1, 1), padding='valid',
+model.add(Conv3D(16, kernel_size=3, strides=(1, 1, 1), padding='valid',
                  data_format='channels_last', dilation_rate=(1, 1, 1),
                  activation='relu', use_bias=True,
                  kernel_initializer='glorot_uniform', bias_initializer='zeros',
