bc433429706aa7807514a706205f4e9b6407db60
Fajar Wimar Ramadhan <wr.fajar@gmail.com>
2019-06-03 11:57:20 +0700

    Add another architecture

---
+++ Arsitektur/Arsitektur11.py
@@ -0,0 +1,50 @@
+#Define a model 11
+model = Sequential()
+model.add(Conv3D(32, kernel_size=5, strides=(2, 2, 2), padding='valid',
+                 data_format='channels_last', dilation_rate=(1, 1, 1),
+                 activation='relu', use_bias=True,
+                 kernel_initializer=keras.initializers.glorot_uniform(seed=0), bias_initializer='zeros',
+                 kernel_regularizer=None, bias_regularizer=None, 
+                 activity_regularizer=None, kernel_constraint=None,
+                 bias_constraint=None, input_shape=(dim1, dim2, dim3, chn)))
+model.add(Conv3D(32, kernel_size=5, strides=(2, 2, 2), padding='valid',
+                 data_format='channels_last', dilation_rate=(1, 1, 1),
+                 activation='relu', use_bias=True,
+                 kernel_initializer=keras.initializers.glorot_uniform(seed=0), bias_initializer='zeros',
+                 kernel_regularizer=None, bias_regularizer=None, 
+                 activity_regularizer=None, kernel_constraint=None,
+                 bias_constraint=None))
+model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1), padding='valid',
+                       data_format='channels_last'))
+model.add(Conv3D(32, kernel_size=3, strides=(1, 1, 1), padding='valid',
+                 data_format='channels_last', dilation_rate=(1, 1, 1),
+                 activation='relu', use_bias=True,
+                 kernel_initializer=keras.initializers.glorot_uniform(seed=0), bias_initializer='zeros',
+                 kernel_regularizer=None, bias_regularizer=None, 
+                 activity_regularizer=None, kernel_constraint=None,
+                 bias_constraint=None, input_shape=(dim1, dim2, dim3, chn)))
+model.add(Conv3D(32, kernel_size=3, strides=(1, 1, 1), padding='valid',
+                 data_format='channels_last', dilation_rate=(1, 1, 1),
+                 activation='relu', use_bias=True,
+                 kernel_initializer=keras.initializers.glorot_uniform(seed=0), bias_initializer='zeros',
+                 kernel_regularizer=None, bias_regularizer=None, 
+                 activity_regularizer=None, kernel_constraint=None,
+                 bias_constraint=None))
+model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1), padding='valid',
+                       data_format='channels_last'))
+model.add(Flatten(data_format='channels_last'))
+model.add(Dense(128, activation='relu', use_bias=True,
+                kernel_initializer=keras.initializers.glorot_uniform(seed=0), bias_initializer='zeros',
+                kernel_regularizer=None, bias_regularizer=None,
+                activity_regularizer=None, kernel_constraint=None,
+                bias_constraint=None))
+model.add(Dense(64, activation='relu', use_bias=True,
+                kernel_initializer=keras.initializers.glorot_uniform(seed=0), bias_initializer='zeros',
+                kernel_regularizer=None, bias_regularizer=None,
+                activity_regularizer=None, kernel_constraint=None,
+                bias_constraint=None))
+model.add(Dense(1, activation=None, use_bias=True,
+                kernel_initializer=keras.initializers.glorot_uniform(seed=0), bias_initializer='zeros',
+                kernel_regularizer=None, bias_regularizer=None,
+                activity_regularizer=None, kernel_constraint=None,
+                bias_constraint=None))
